3060af6ba04592281ae967770e47cc9912ea439e
Damien <90845231+Damien-Dupont@users.noreply.github.com>
2026-03-30 22:24:51 +0200

    ADDED second test for useCreateTransition, fixed first one

--- front/src/hooks/useCreateTransition.test.ts
+++ front/src/hooks/useCreateTransition.test.ts
@@ -1,22 +1,38 @@
-import { renderHook, waitFor } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { useCreateTransition } from "./useCreateTransition";
 
 describe("useCreateTransition", () => {
-  it("calls onTransitionCreate with proper scene_id when a scene is selected for transition", async () => {
+  it("returns success status after calling createTransition", async () => {
     globalThis.fetch = vi.fn().mockResolvedValue({
       ok: true,
+      json: async () => ({ status: "ok", data: {} }),
+    });
+
+    const { result } = renderHook(() => useCreateTransition("lalala"));
+
+    await act(async () => {
+      await result.current.createTransition("bububu", "Aller dans la forêt");
+    });
+
+    expect(result.current.status).toBe("success");
+  });
+
+  it("returns error status after calling createTransition return API error", async () => {
+    globalThis.fetch = vi.fn().mockResolvedValue({
+      ok: false,
       json: async () => ({
-        status: "ok",
-        data: { sceneId: "lalala", sceneToLink: "bububu" },
+        status: "error",
+        error: "Erreur lors de la création de transition",
       }),
     });
-    const { result } = renderHook(() =>
-      useCreateTransition("lalala", "bububu"),
-    );
 
-    await waitFor(() => {
-      expect(result.current).toBe("ok");
+    const { result } = renderHook(() => useCreateTransition("lalala"));
+
+    await act(async () => {
+      await result.current.createTransition("bububu", "Aller dans la forêt");
     });
+
+    expect(result.current.status).toBe("error");
   });
 
   // it("", ()=>{})
--- front/src/hooks/useCreateTransition.ts
+++ front/src/hooks/useCreateTransition.ts
@@ -1,4 +1,32 @@
-export function useCreateTransition(sceneId: string, sceneToLink: string) {
-  // order des deux scènes // order general // label
+import { useState } from "react";
+
+type SaveStatus = "idle" | "success" | "error";
+
+export function useCreateTransition(sceneId: string) {
+  const [status, setStatus] = useState<SaveStatus>("idle");
+  const [error, setError] = useState<string | null>(null);
+
+  const createTransition = async (
+    forwardScene: string,
+    forwardLabel: string | null,
+  ) => {
+    try {
+      const res = await fetch(`http://localhost:8080/transitions/${sceneId}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ forwardScene, forwardLabel }),
+      });
+      if (!res.ok) throw new Error("Erreur API");
+      setStatus("success");
+    } catch {
+      setStatus("error");
+      setError("Erreur lors de la création de transition");
+    }
+  };
+  return {
+    createTransition,
+    error,
+    status,
+  };
 }
 // order des deux scènes // order general // label
